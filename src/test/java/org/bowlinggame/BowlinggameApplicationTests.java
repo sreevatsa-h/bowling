41dbb3e91a151dc4cd706ae0680c48b05094e7ed
Sreevatsa <sreevatsa@trabacus.com>
2021-02-08 14:37:05 +0530

    first commit

---
+++ src/test/java/org/bowlinggame/BowlinggameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bowlinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlinggameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
